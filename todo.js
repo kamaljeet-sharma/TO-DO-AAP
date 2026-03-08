896ce48cbdae063e97c9dd9003df8334bc546ca5
Kamaljeet Sharma <kamaljeetsharma2020@gmail.com>
2026-03-08 16:09:31 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,25 @@
+//todo 
+let btn = document.querySelector("button");
+let inp = document.querySelector("input");
+let ul = document.querySelector("ul");
+
+btn.addEventListener("click", function () {
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value = "";
+});
+
+ul.addEventListener("click", function (event) {
+    if (event.target.classList.contains("delete")) {
+        let li = event.target.parentElement;
+        ul.removeChild(li);
+        console.log("deleted");
+    }
+});
